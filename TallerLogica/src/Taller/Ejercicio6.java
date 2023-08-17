196dc9f955d2e2fb3dc2eb7e86398469415bfe11
manubc2012 <139500169+manubc2012@users.noreply.github.com>
2023-08-17 13:40:25 -0500

    Create Ejercicio6.java

---
+++ TallerLogica/src/Taller/Ejercicio6.java
@@ -0,0 +1,9 @@
+package Taller;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+      for (int i = 2; i <= 200; i += 2) {
+            System.out.println(i);
+      }
+  }
+}
